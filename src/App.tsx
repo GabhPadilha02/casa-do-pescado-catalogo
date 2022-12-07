7b549f374753f4e9bc1c7c29c66c68be9fc9d24b
gabhpadilha02 <gabh.padilha2@gmail.com>
2022-12-07 10:32:44 -0300

    remove themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { Header } from './components/Header'
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/deafult'
 import { GlobalStyle } from './styles/global'
 import { MainSearchContainer } from './components/MainSearchContainer'
 import { Category } from './components/Category'
@@ -10,13 +8,11 @@ import { AppContextProvider } from './contexts/AppContext'
 export function App() {
   return (
     <AppContextProvider>
-      <ThemeProvider theme={defaultTheme}>
-        <Header />
-        <MainSearchContainer />
-        <Category />
-        <Product />
-        <GlobalStyle />
-      </ThemeProvider>
+      <Header />
+      <MainSearchContainer />
+      <Category />
+      <Product />
+      <GlobalStyle />
     </AppContextProvider>
   )
 }
