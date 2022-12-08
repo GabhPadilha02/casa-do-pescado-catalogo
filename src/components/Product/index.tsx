024813e376628af1ca5715f07a051b6d00f6a97c
gabhpadilha02 <gabh.padilha2@gmail.com>
2022-12-08 11:07:29 -0300

    create modal function to img of procuts

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,7 +1,11 @@
-import { useContext } from 'react'
+import { X } from 'phosphor-react'
+import { useContext, useState } from 'react'
 import { AppContext } from '../../contexts/AppContext'
 import {
   Description,
+  Model,
+  ModelOpen,
+  Pics,
   ProductContainer,
   ProductInfosContainer,
   ProductItem,
@@ -12,22 +16,45 @@ import {
 export function Product() {
   const { filteredProducts } = useContext(AppContext)
 
+  const [model, setModel] = useState(false)
+  const [tempImgSrc, setTempImgSrc] = useState('')
+
+  const getImg = (url: string) => {
+    setTempImgSrc(url)
+    setModel(true)
+  }
+
   return (
-    <ProductContainer id="products">
-      {filteredProducts.map((a: any) => (
-        <ProductItem key={a.title}>
-          <ProductInfosContainer>
-            {a.imageOfProduct.map((i: any) => (
-              <img src={i.url} key={i.url} alt="" />
-            ))}
-            <TextContainer>
-              <h2 key={a.title}>{a.title}</h2>
-              <Description key={a.subtitle}>{a.subtitle}</Description>
-              <WeightProduct key={a.weight}>{a.weight}</WeightProduct>
-            </TextContainer>
-          </ProductInfosContainer>
-        </ProductItem>
-      ))}
-    </ProductContainer>
+    <>
+      {model ? (
+        <ModelOpen>
+          <img src={tempImgSrc} alt="Model" />
+          <X onClick={() => setModel(false)} />
+        </ModelOpen>
+      ) : (
+        <Model>
+          <img src={tempImgSrc} alt="Model" />
+          <X onClick={() => setModel(false)} />
+        </Model>
+      )}
+      <ProductContainer id="products">
+        {filteredProducts.map((a: any) => (
+          <ProductItem key={a.title}>
+            <ProductInfosContainer>
+              {a.imageOfProduct.map((i: any) => (
+                <Pics key={i.url} onClick={() => getImg(i.url)}>
+                  <img src={i.url} key={i.url} alt="" />
+                </Pics>
+              ))}
+              <TextContainer>
+                <h2 key={a.title}>{a.title}</h2>
+                <Description key={a.subtitle}>{a.subtitle}</Description>
+                <WeightProduct key={a.weight}>{a.weight}</WeightProduct>
+              </TextContainer>
+            </ProductInfosContainer>
+          </ProductItem>
+        ))}
+      </ProductContainer>
+    </>
   )
 }
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -22,8 +22,8 @@ export const ProductItem = styled.div`
   align-items: center;
   justify-content: start;
   padding: 2rem;
-  width: 200px;
-  max-height: 260px;
+  width: 240px;
+  /* max-height: 260px; */
   background-color: #ffffff;
   background-image: url('https://www.transparenttextures.com/patterns/az-subtle.png');
   border-radius: 8px;
@@ -32,28 +32,21 @@ export const ProductItem = styled.div`
   box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.5);
 
   @media (max-width: 700px) {
-    width: 150px;
-    height: 200px;
+    width: 180px;
+    height: 350px;
   }
 `
 
 export const ProductInfosContainer = styled.div`
   display: flex;
   flex-direction: column;
-  position: relative;
+  /* position: relative; */
   bottom: 7rem;
   align-items: center;
   justify-content: center;
 
-  img {
-    height: 140px;
-  }
-
   @media (max-width: 700px) {
     bottom: 5rem;
-    img {
-      height: 80px;
-    }
   }
 `
 
@@ -78,7 +71,7 @@ export const TextContainer = styled.div`
   @media (max-width: 700px) {
     text-align: center;
     width: 130px;
-    position: relative;
+    /* position: relative; */
     top: 0rem;
 
     h2 {
@@ -89,7 +82,7 @@ export const TextContainer = styled.div`
 
 export const Description = styled.span`
   height: 40px;
-  position: relative;
+  /* position: relative; */
   left: 0;
   width: 80%;
   font-weight: 700;
@@ -105,5 +98,87 @@ export const WeightProduct = styled.span`
 
   @media (max-width: 700px) {
     font-size: 1rem;
+  }
+`
+export const Pics = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  -webkit-transition: all 0.2s ease;
+  transition: all 0.2s ease;
+  cursor: pointer;
+  margin-bottom: 12px;
+  position: static;
+
+  &:hover {
+    filter: opacity(0.9);
+  }
+
+  img {
+    height: 140px;
+    max-width: 150px;
+    /* object-fit: cover; */
+
+    @media (max-width: 700px) {
+      img {
+        height: 50px;
+      }
+    }
+  }
+`
+
+export const Model = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  background-color: red;
+  transition: opacity 0.4s ease visibility 0.4s ease transform 0.5s ease-in-out;
+  display: none;
+  opacity: 0;
+  transform: scale(0);
+  overflow: hidden;
+
+  img {
+    display: block;
+    line-height: 0;
+    box-sizing: border-box;
+    padding: 20px 0 20px;
+    margin: 0 auto;
+    max-height: 70vh;
+  }
+`
+
+export const ModelOpen = styled.div`
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  visibility: visible;
+  opacity: 1;
+  transform: scale(1);
+  top: 0;
+  left: 0;
+
+  img {
+    max-height: 80vh;
+  }
+
+  svg {
+    position: fixed;
+    top: 10px;
+    right: 30px;
+    width: 2rem;
+    height: 2rem;
+    padding: 5px;
+    background-color: rgba(0, 0, 0, 0.4);
+    color: #fff;
+    cursor: pointer;
   }
 `
